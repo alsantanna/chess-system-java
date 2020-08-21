9ca3400621197948a059aad3fdd910af08e2305e
Alex <al_santanna@yahoo.com>
2020-08-20 23:23:52 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,6 +26,7 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
+	
 	public void initialSetup() {
 		
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));	
